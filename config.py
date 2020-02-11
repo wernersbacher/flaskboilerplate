20e00d8906308a11a8f346c8cb52312bb5c69350
wernersbacher <wernersbacher@gmail.com>
2020-02-11 14:36:37 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'Mein-Schlüssel-ist-super-sicher.bitte-nicht-ändern'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or 'edit-this-key'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
